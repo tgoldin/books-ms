fba94529cf803cd72ecca08d415eb30fb3e9ba6b
tgoldin <tpg@blueyonder.co.uk>
2020-06-11 09:04:46 +0100

    Project initialised.

---
+++ src/main/java/com/tpg/booksms/BooksMsApplication.java
@@ -0,0 +1,13 @@
+package com.tpg.booksms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksMsApplication.class, args);
+	}
+
+}
